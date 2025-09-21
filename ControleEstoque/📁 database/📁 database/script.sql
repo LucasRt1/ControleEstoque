c54f42d4e2db2f5e1ee3fc6983a84b0f73665f1b
LucasRt1 <lucasrafaeltobias123@gmail.com>
2025-09-21 18:14:47 -0300

    Update script.sql

--- ControleEstoque/📁 database/📁 database/script.sql
+++ ControleEstoque/📁 database/📁 database/script.sql
@@ -1,6 +1,13 @@
+CREATE DATABASE controle_estoque;
+
 CREATE TABLE produto (
     id_produto SERIAL PRIMARY KEY,
     nome VARCHAR(45) NOT NULL,
     preco DECIMAL(10,2) NOT NULL,
     quantidade INT DEFAULT 0
 );
+
+INSERT INTO produto (nome, preco, quantidade) VALUES
+('Notebook Dell i7', 3500.00, 15),
+('Mouse Gamer RGB', 120.50, 30),
+('Teclado Mecânico', 250.00, 20);
